d6af63a47f7bdebad8bfb9fe2f9a2386ef1e46f4
Greeshma <greeshmachowdary98@gmail.com>
2025-12-08 04:08:32 +0000

    Added new echo statement in myDeploy

--- vars/myDeploy.groovy
+++ vars/myDeploy.groovy
@@ -1,5 +1,8 @@
 def call(String serviceName) {
     echo "===== DEPLOY STAGE ====="
     echo "Deploying: ${serviceName}"
+
+    echo ">>> NEW UPDATE: Shared Library Change Applied! <<<"
+
     sh "echo Simulating deployment for ${serviceName}"
 }
